79a42cd8a46f335518a06ffb8269515f78fcde73
Aretha Kebirungi <akebirungi@alumni.cmu.edu>
2018-10-12 18:37:25 +0300

    allow only one correct answer on question creation

--- app/graphql/resolvers/create_question.rb
+++ app/graphql/resolvers/create_question.rb
@@ -5,14 +5,19 @@ class Resolvers::CreateQuestion < GraphQL::Function
 
 	type Types::QuestionType
 
+	def only_one_correct?(answers)
+		num_correct = 0
+		answers.each { |answer| num_correct += 1 if answer[:correct] }
+		num_correct == 1
+	end	
+
 	def call(_obj, args, _ctx)
 		question = Question.create!(name: args[:name],
 											 category: Category.find_by(name: args[:category]))
 		
-		args[:answers].each do |answer|
-			question.answers.create!(name: answer[:name], correct: answer[:correct])
-		end
-
+		raise GraphQL::ExecutionError.new "Multiple answers flagged as correct, question may only have one correct answer" unless only_one_correct?(args[:answers])
+		args[:answers].each { |answer| question.answers.create!(name: answer[:name], correct: answer[:correct]) }
+		
 		question
 	end
 
--- spec/graphql/resolvers/create_question_spec.rb
+++ spec/graphql/resolvers/create_question_spec.rb
@@ -24,4 +24,21 @@ RSpec.describe Resolvers::CreateQuestion do
 		end
 	
 	end
+
+	context "invalid input for question fields" do
+		let(:args) do 
+			{ name: "What was the first planet to be discovered using the telescope, in 1781?",
+	  			category: "mcq",
+				answers: [{ name: "Pluto", correct: false },
+				 				{ name: "Uranus", correct: true },
+								{ name: "Pluto", correct: false },
+								{ name: "Jupiter", correct: true }]
+			}
+		end
+
+		it "throws error if more than one answer is correct" do
+			expect { new_question }.to raise_error(GraphQL::ExecutionError) 
+		end
+		
+	end
 end
