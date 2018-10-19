aa9b86639faf39ff43f52aac356bd798376f920e
Aretha Kebirungi <akebirungi@alumni.cmu.edu>
2018-10-19 18:01:22 +0300

    test case for mongoid question input validation

--- spec/graphql/resolvers/question_create_spec.rb
+++ spec/graphql/resolvers/question_create_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Resolvers::CreateQuestion do
+RSpec.describe Resolvers::QuestionCreate do
  	subject(:new_question) { described_class.new.call(_obj, args, _ctx) }
 
 	let(:_obj) { {} }
 	let(:_ctx) { {} }
 
-	context "valid input for question fields" do
+	context "valid input for all question fields" do
 		
 		let(:args) do 
 			{ name: "What was the first planet to be discovered using the telescope, in 1781?",
@@ -25,7 +25,7 @@ RSpec.describe Resolvers::CreateQuestion do
 	
 	end
 
-	context "invalid input for question fields" do
+	context "invalid answers input for question" do
 		let(:args) do 
 			{ name: "What was the first planet to be discovered using the telescope, in 1781?",
 	  			category: "mcq",
@@ -40,5 +40,21 @@ RSpec.describe Resolvers::CreateQuestion do
 			expect { new_question }.to raise_error(GraphQL::ExecutionError) 
 		end
 		
+	end
+
+	context "null input for required question name field" do
+		let(:args) do
+			{ name: "",
+	  			category: "open",
+				answers: [{ name: "Pluto", correct: false },
+				 				{ name: "Uranus", correct: false },
+								{ name: "Pluto", correct: false },
+								{ name: "Jupiter", correct: true }]
+			}
+		end
+
+		it "throws error if question name is null" do
+			expect { new_question }.to raise_error(GraphQL::ExecutionError)
+		end
 	end
 end
