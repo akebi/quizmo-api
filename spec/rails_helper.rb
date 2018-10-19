2096299c2151627749a53d1c0c49fa6fd589709e
Aretha Kebirungi <akebirungi@alumni.cmu.edu>
2018-10-19 18:06:24 +0300

    spec test case for question record creation validation errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,8 +63,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
 
-RSpec.configure do |config|
+  # Mongoid matchers for spec tests
   config.include Mongoid::Matchers, type: :model
+
 end
