cc907acbd3296045500d978b4dd790b7faee2b18
Aretha Kebirungi <akebirungi@alumni.cmu.edu>
2018-10-23 20:21:10 +0300

    add error class variable getters

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,10 @@
 module QuestionsHelper
 	class Errors
-		INVALID_INPUT = "Invalid question input"
+		@@INVALID_INPUT = "Invalid question input"
+	
+		def self.INVALID_INPUT
+			@@INVALID_INPUT 		
+		end
+
 	end
 end
